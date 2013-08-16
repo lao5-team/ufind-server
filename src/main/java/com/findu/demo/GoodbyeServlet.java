c842ef81141ec0bbbd53075620cc25e288fbd9b5
yafei.zhang <zhangyafeikimi@gmail.com>
2013-08-17 02:14:27 +0800

    INIT

---
+++ src/main/java/com/findu/demo/GoodbyeServlet.java
@@ -0,0 +1,27 @@
+package com.findu.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GoodbyeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private String msg = "Goodbye!";
+
+    public GoodbyeServlet() {
+    }
+
+    public GoodbyeServlet(String msg) {
+        this.msg = msg;
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>" + msg + "</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+}
+
